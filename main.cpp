0a0d71740262152346afa8947e29817269654cf0
Kez <100411374+21kezia@users.noreply.github.com>
2022-05-17 20:36:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
-#include "gameManager.h"
-#include "board.h"
 #include <iostream>
-using namespace std;
+#include "GameManager.h"
 
-int main()
-{
-    board b;
-    gameManager gm;
+int main() {
+	Board sudoku;
+	Player player;
+	GameManager gm(&sudoku, &player);
 
-    gm.play();
+	int menuOpt = 0;
+	while (menuOpt != 99)
+	{
+		cout << "---Menu---" << endl
+			<< "1. Play" << endl
+			<< "2. Contibue " << endl
+			<< "3. Set Player Name" << endl
+			<< "4. Save" << endl
+			<< "5. Load" << endl
+			<< "99. Exit" << endl
+			<< "[] : "; cin >> menuOpt;
+
+		switch (menuOpt)
+		{
+		case 1:
+			gm.play();
+			break;
+		case 2:
+			gm.continueGame();
+			break;
+		case 3:
+			gm.setPlayerUsername();
+			break;
+		case 4:
+			gm.saveFile();
+			break;
+		case 5:
+			gm.loadFile();
+			break;
+		case 99:
+			break;
+		default:;
+		}
+	}
 }
