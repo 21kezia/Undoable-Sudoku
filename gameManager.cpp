7314db652a3101b4c849b8e4837eb42862aac45f
Kez <100411374+21kezia@users.noreply.github.com>
2022-05-17 20:18:24 +0700

    Update gameManager.cpp

--- gameManager.cpp
+++ gameManager.cpp
@@ -1,57 +1,176 @@
-#include "gameManager.h"
+#include "GameManager.h"
 
-gameManager::gameManager() {
-    
+GameManager::GameManager(Board* board, Player* player)
+{
+	this->board = board;
+	this->player = player;
 }
-gameManager::gameManager(player Player, board Board) {
-	b = Board;
-	p = Player;
+
+void GameManager::checkWrongCell()
+{
+	struct wrongCell
+	{
+		int x;
+		int y;
+		int value;
+	};
+
+	vector <wrongCell> Cell;
+
+	for (int i = 0; i < GRID_SIZE; ++i) {
+		for (int j = 0; j < GRID_SIZE; ++j) {
+			if (this->board->getCellValue(i, j) != this->board->getCellSolValue(i, j)) {
+				Cell.push_back({ i, j, this->board->getCellValue(i,j) });
+			}
+		}
+	}
+
+	cout << endl
+		<< "Every cell are correct exept for these : " << endl;
+	for (const auto& arr : Cell)
+	{
+		cout << "Cell "
+			<< "[" << arr.x << "]"
+			<< "[" << arr.y << "] : ";
+		if (arr.value == 0)
+		{
+			cout << "Still Empty";
+		}
+		else {
+			cout << arr.value;
+		}
+		cout << endl;
+		Cell.pop_back();
+	}
 }
-void gameManager::play() {
-    b.boardLoader("sudokuTxt");
 
-    if (p.getUsername() == "") {
-        cout << "Insert username: ";
-        cin >> username;
-        p.setUsername(username);
-    }
-
-    while (!checkWin())
-    {
-        int row, col, value;
-        system("cls");
-        b.displayBoard();
-
-        cout << endl << endl;
-        cout << "Insert row: ";
-        cin >> row;
-        cout << "Insert column: ";
-        cin >> col;
-        cout << "Insert value (1-9): ";
-        cin >> value;
-
-        b.setValueAtIndex(row - 1, col - 1, value);
-
-    }
-
-    system("cls");
-    b.displayBoard();
-
-    cout << "Congratulations! You have completed the Sudoku.\n\n";
-
-    p.setWinCount(p.getWinCount() + 1);
-    p.setMatchCount(p.getMatchCount() + 1);
-
-    cout << "Stat: \n";
-
-    cout << p.getUsername() << endl;
-    cout << "Match Total: " << p.getMatchCount() << endl;
-    cout << "Win Total: " << p.getWinCount() << endl << endl;
+void GameManager::saveFile()
+{
+	SaveLoad save;
+	string filename;
+	save.showSaveList();
+	cout << endl << "Save as : "; cin >> filename;
+	save.Save(board, filename);
 }
-bool gameManager::checkWin() {
-	if (b.isValidConfig() && b.isFull()) {
-		return true;
+
+void GameManager::loadFile()
+{
+	SaveLoad load;
+	string filename;
+	load.showSaveList();
+	cout << endl << "Load from : "; cin >> filename;
+	load.Load(board, filename);
+}
+
+void GameManager::setPlayerUsername()
+{
+	string username;
+	cout << endl
+		<< "Insert username : "; cin >> username;
+	player->setUsername(username);
+}
+
+void GameManager::play()
+{
+	cout << endl << "Set Difficulty, how many cell to remove : ";
+	int r = 0; cin >> r;
+	this->board->difficulty(r);
+	board->generate();
+	continueGame();
+}
+
+void GameManager::continueGame()
+{
+	int op = 0;
+	int x, y, z;
+	while (op != 99)
+	{
+		if (this->board->isSolRight())
+		{
+			cout << endl << "Congratulations!!" << endl
+				<< "you have finished the sudoku with "
+				<< (undo.getSize() + redo.getSize())
+				<< " action!" << endl << endl;
+			break;
+		}
+		board->draw();
+		cout << endl
+			<< "[1] Fill" << endl
+			<< "[2] Delete " << endl
+			<< "[3] Undo " << endl
+			<< "[4] Redo " << endl
+			<< "[5] Check Wrong Cell" << endl
+			<< "[99] Exit" << endl
+			<< "[] : "; cin >> op;
+		switch (op)
+		{
+		case 1:
+			cout << "X     :"; cin >> x;
+			cout << "Y     :"; cin >> y;
+			cout << "Value :"; cin >> z;
+			fillCell(x, y, z);
+			break;
+		case 2:
+			cout << "X     :"; cin >> x;
+			cout << "Y     :"; cin >> y;
+			deleteCell(x, y);
+			break;
+		case 3:
+			undoAct();
+			break;
+		case 4:
+			redoAct();
+			break;
+		case 5:
+			checkWrongCell();
+		default:;
+		}
+	}
+}
+
+void GameManager::undoAct()
+{
+	Command* a;
+	a = &undo.pop();
+	cout << a->getType();
+	redo.push(a);
+	a->undo();
+
+	cout << "Undo action on cell ["
+		<< a->getQuery(0) << "]["
+		<< a->getQuery(1) << "]" << endl;
+}
+
+void GameManager::redoAct()
+{
+	Command* a;
+	a = &redo.pop();
+	undo.push(a);
+	a->redo();
+	cout << "Redo action on cell ["
+		<< a->getQuery(0) << "]["
+		<< a->getQuery(1) << "]" << endl;
+}
+
+void GameManager::fillCell(int x, int y, int value)
+{
+	Fill* a = new Fill;
+	a->setBoard(board);
+	if (board->getCellValue(x, y) != 0)
+	{
+		cout << endl << "Cell already has value!" << endl;
 	}
 	else
-		return false;
+	{
+		a->execute(x, y, value);
+	}
+	undo.push(a);
+}
+
+void GameManager::deleteCell(int x, int y)
+{
+	Delete* a = new Delete;
+	a->setBoard(board);
+	undo.push(a);
+	a->execute(x, y);
 }
